3aa3f2bb862091ec1e1d991e68f1a43ca298612d
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-11-05 11:07:24 -0400

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,11 @@ class Student
     attr_reader :hello, :raise_hand
 
     def hello 
-        puts "Hey there! I'm so excited to learn stuff.\n"
+        puts "Hey there! I'm so excited to learn stuff."
     end
 
     def raise_hand
-        puts "Pick me!\n"
+        puts "Pick me!"
     end
 
 end
