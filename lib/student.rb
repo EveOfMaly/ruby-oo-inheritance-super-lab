e73c940487c0ac3d4d020dd665c52f075b54c66b
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-11-05 11:06:12 -0400

    completed assignment

---
+++ lib/student.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Student
+
+    attr_reader :hello, :raise_hand
+
+    def hello 
+        puts "Hey there! I'm so excited to learn stuff.\n"
+    end
+
+    def raise_hand
+        puts "Pick me!\n"
+    end
+
+end
+
